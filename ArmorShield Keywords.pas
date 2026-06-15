8cbabee0e4053ff5f48006cdcbb44a8ae7196c57
Guizz <guizzoni@gmail.com>
2026-06-15 13:39:01 -0300

    Update ArmorShield Keywords.pas
    add checks, fix keyword replacement

--- ArmorShield Keywords.pas
+++ ArmorShield Keywords.pas
@@ -1,15 +1,11 @@
-{
-  Add Keywords for ArmorShield_Heavy/ArmorShield_Light as necessary
-  Created by Guizz
-  v1.0
-}
-
 unit UserScript;
 
 function Process(e: IInterface): integer;
 var
-  flagsElement: IInterface;
+  flagsElement, kwda, newKw, kwItem: IInterface;
   armorType: string;
+  targetKwString: string;
+  i: integer;
 begin
   Result := 0;
 
@@ -18,14 +14,33 @@ begin
   flagsElement := ElementByPath(e, 'BOD2\First Person Flags\39 - Shield');
   
   if Assigned(flagsElement) and (GetEditValue(flagsElement) = '1') then begin
-    AddMasterIfMissing(GetFile(e), 'Update.esm');
     armorType := GetElementEditValues(e, 'BOD2\Armor Type');
     
     if armorType = 'Heavy Armor' then
-      SetEditValue(ElementByPath(e, 'KWDA\Keyword'), 'ArmorShield_Heavy [KYWD:01CD112B]');
+      targetKwString := 'ArmorShield_Heavy [KYWD:01CD112B]'
+    else if armorType = 'Light Armor' then
+      targetKwString := 'ArmorShield_Light [KYWD:01CD112C]'
+    else 
+      Exit;
 
-    if armorType = 'Light Armor' then
-      SetEditValue(ElementByPath(e, 'KWDA\Keyword'), 'ArmorShield_Light [KYWD:01CD112C]');
+    kwda := ElementByPath(e, 'KWDA');
+    if Assigned(kwda) then begin
+      for i := 0 to ElementCount(kwda) - 1 do begin
+        kwItem := ElementByIndex(kwda, i);
+        if GetEditValue(kwItem) = targetKwString then 
+          Exit;
+      end;
+    end else begin
+      kwda := Add(e, 'KWDA', True);
+    end;
+
+    AddMasterIfMissing(GetFile(e), 'Update.esm');
+    
+    if Assigned(kwda) then begin
+      newKw := ElementAssign(kwda, HighInteger, nil, False);
+      if Assigned(newKw) then
+        SetEditValue(newKw, targetKwString);
+    end;
   end;
 end;
 
